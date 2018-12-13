e7921e58a807de1e4ec858398ee2d6c332a8ae38
MANUEL HERNANDEZ <rl106926@rlas-macair-36.local>
2018-12-13 11:33:22 -0600

    Initial Commit

---
+++ ImageView/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  ImageView
+//
+//  Created by MANUEL HERNANDEZ on 9/6/18.
+//  Copyright © 2018 MANUEL HERNANDEZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    var myImage = #imageLiteral(resourceName: "kisspng-chanel-t-shirt-louis-vuitton-logo-monogram-gucci-logo-5ac3e9d2d442d0.9789954115227888188694")
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imageView.image = myImage
+    }
+
+   
+
+
+}
+
